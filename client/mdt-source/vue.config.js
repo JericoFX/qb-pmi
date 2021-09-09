a382b4c2b026fd73b3ecb581c2a4dd8d7f3d3cba
Floris <nagsterza@gmail.com>
2021-09-09 14:34:19 +0200

    Project structure

---
+++ client/mdt-source/vue.config.js
@@ -0,0 +1,8 @@
+const path = require("path");
+module.exports = {
+  transpileDependencies: [
+    'vuetify'
+  ],
+  outputDir: path.resolve(__dirname, "../html"),
+  //assetsDir: "../../static/SPA"
+}
